f777998c3002ee7712bdc578ae76beabdb630333
Werner <werner_a_e@yahoo.es>
2016-02-19 15:42:53 -0430

    rename yesterday to prev_day and tomorrow to next_day

--- src/crystal_on_steroids/time.cr
+++ src/crystal_on_steroids/time.cr
@@ -1,11 +1,21 @@
 struct Time
   # Moves one day in the past.
-  def yesterday
+  def prev_day
     self - 1.day
   end
 
   # Moves one day in the future.
-  def tomorrow
+  def next_day
     self + 1.day
+  end
+
+  # Moves one day in the past.
+  def yesterday
+    prev_day
+  end
+
+  # Moves one day in the future.
+  def tomorrow
+    next_day
   end
 end
