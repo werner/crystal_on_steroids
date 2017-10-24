4a52ede1393f540b0588545732eee9362c9e442e
Harisankar P S <mailme@hsps.in>
2017-10-24 01:37:05 -0700

    Refactor: in_groups method with block to get data from method without block

--- src/crystal_on_steroids/array.cr
+++ src/crystal_on_steroids/array.cr
@@ -179,21 +179,8 @@ class Array(T)
   end
 
   def in_groups(number, fill_with = nil)
-    division = size / number
-    modulo = size % number
+    groups = in_groups(number, fill_with)
 
-    groups = [] of Array(T | Nil | Bool)
-    start = 0
-
-    number.times do |index|
-      last_group = [] of T | Nil | Bool
-      length = division + (modulo > 0 && modulo > index ? 1 : 0)
-      self[start..(start + (length - 1))].each { |e| last_group << e }
-      groups << last_group
-      last_group.push(fill_with) if fill_with != false &&
-                                    modulo > 0 && length == division
-      start += length
-    end
     groups.each { |g| yield(g) }
   end
 end
